b05ed8ebf4adc2456e8c1f06b3f6df304988b40e
“Damir” <dami_rkomp@gmail.com>
2024-12-09 18:59:50 +0300

    изменено оформление окна регистрации (фон, чекбоксы и кнопки были перекрашены в другой цвет)

--- lebedevReg2/Form1.Designer.cs
+++ lebedevReg2/Form1.Designer.cs
@@ -41,53 +41,60 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.button1.Location = new System.Drawing.Point(256, 375);
+            this.button1.Location = new System.Drawing.Point(384, 577);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 45);
+            this.button1.Size = new System.Drawing.Size(202, 69);
             this.button1.TabIndex = 0;
             this.button1.Text = "Регистрация";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.button2.Location = new System.Drawing.Point(256, 466);
+            this.button2.Location = new System.Drawing.Point(384, 717);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 45);
+            this.button2.Size = new System.Drawing.Size(202, 69);
             this.button2.TabIndex = 1;
             this.button2.Text = "Выход";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label1.Location = new System.Drawing.Point(242, 89);
+            this.label1.Location = new System.Drawing.Point(363, 137);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(173, 43);
+            this.label1.Size = new System.Drawing.Size(260, 66);
             this.label1.TabIndex = 2;
             this.label1.Text = "Регистрация";
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label2.Location = new System.Drawing.Point(114, 156);
+            this.label2.Location = new System.Drawing.Point(171, 240);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 43);
+            this.label2.Size = new System.Drawing.Size(138, 66);
             this.label2.TabIndex = 3;
             this.label2.Text = "ФИО";
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label3.Location = new System.Drawing.Point(114, 208);
+            this.label3.Location = new System.Drawing.Point(171, 320);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 43);
+            this.label3.Size = new System.Drawing.Size(150, 66);
             this.label3.TabIndex = 4;
             this.label3.Text = "Пароль";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -95,48 +102,53 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.textBox1.Location = new System.Drawing.Point(231, 162);
+            this.textBox1.Location = new System.Drawing.Point(346, 249);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(160, 23);
+            this.textBox1.Size = new System.Drawing.Size(238, 31);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.textBox2.Location = new System.Drawing.Point(231, 214);
+            this.textBox2.Location = new System.Drawing.Point(346, 329);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(160, 23);
+            this.textBox2.Size = new System.Drawing.Size(238, 31);
             this.textBox2.TabIndex = 6;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // checkedListBox1
             // 
-            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.checkedListBox1.BackColor = System.Drawing.Color.MistyRose;
             this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.checkedListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
             this.checkedListBox1.FormattingEnabled = true;
             this.checkedListBox1.Items.AddRange(new object[] {
             "Муж.",
             "Жен."});
-            this.checkedListBox1.Location = new System.Drawing.Point(231, 260);
+            this.checkedListBox1.Location = new System.Drawing.Point(346, 400);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(114, 42);
+            this.checkedListBox1.Size = new System.Drawing.Size(171, 64);
             this.checkedListBox1.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label4.Location = new System.Drawing.Point(114, 259);
+            this.label4.Location = new System.Drawing.Point(171, 398);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(100, 43);
+            this.label4.Size = new System.Drawing.Size(150, 66);
             this.label4.TabIndex = 8;
             this.label4.Text = "Пол";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(641, 599);
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(962, 922);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.textBox2);
@@ -146,6 +158,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
